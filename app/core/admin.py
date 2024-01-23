5de4be779041fe6f6e978eb99f2babd381c0ae95
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-01-23 15:20:53 +0000

    adding models to the admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -45,3 +45,8 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Category)
+admin.site.register(models.Product)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
